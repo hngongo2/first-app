fe67322bf705b3ddb651b74d0f5826142577dfa1
hngongo2 <hngongo2@gmail.com>
2021-04-12 23:51:49 +0200

    mise a jour

---
+++ app/src/main/java/com/example/map_cadastrelushi/MapView.java
@@ -0,0 +1,17 @@
+package com.example.map_cadastrelushi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+
+public class MapView extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_view);
+    }
+}
